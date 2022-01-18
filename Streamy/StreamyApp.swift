8683f837856080a26abf5b1bff68fa410fa5a8f2
Ryan Ferrell <importRyan@gmail.com>
2022-01-17 17:04:38 -0800

    Initial Commit

---
+++ Streamy/StreamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamyApp.swift
+//  Streamy
+//
+//  Created by Ryan Ferrell on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct StreamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
